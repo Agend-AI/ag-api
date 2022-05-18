6785e3d385eac3e5ea08b2b64119e154c80338ce
pedro-limma <pedro.lima.wk@gmail.com>
2022-05-17 21:04:55 -0300

    feat: deploy finalizado

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes"));
 
-app.listen(3000, () => console.log(`Example app listening on port 3000`));
+app.listen(process.env.PORT || 3000);
